02dd78a613b249c63f454504fcf6bd2e0e6e9588
francesca <francesca.sinigaglia91@gmail.com>
2024-04-10 18:34:15 +0200

    First push

---
+++ src/main/java/it/francesca/progettostudio/ProgettostudioApplication.java
@@ -0,0 +1,13 @@
+package it.francesca.progettostudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettostudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettostudioApplication.class, args);
+	}
+
+}
